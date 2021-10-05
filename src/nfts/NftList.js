9fa5388940a979534d31e8bc4c5b9b6ec11f1edd
Bryan Holbrook <bryandholbrook@gmail.com>
2021-10-05 14:05:22 -0700

    Updated styles and logic. Typescript template remaining and minor clean up.

--- src/nfts/NftList.js
+++ src/nfts/NftList.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import NftData from '../data/test_data.json';
-import { Box, Center, Text, Grid, GridItem, SimpleGrid, Square, Circle } from "@chakra-ui/react"
+import { Box, Center, Flex, Spacer, Grid, GridItem, SimpleGrid, Circle } from "@chakra-ui/react"
+
+const makeColors = ["#2115A9 10.54%, #FFFFFF 132.1%", "#52FF00 10.54%, #FFFFFF 132.1%", "#35FFCF 10.54%, #FFFFFF 132.1%", "#FFC93D 10.54%, #FFFFFF 132.1%"]
+const takeColors = ["#48FFA7 10.54%, #FFFFFF 132.1%", "#FFC93D 10.54%, #FFFFFF 132.1%", "#1ABBEE 10.54%, #FFFFFF 132.1%", "#EE1A1A 10.54%, #FFFFFF 132.1%"]
 
 export default function NftList () {
-	const makeColors = ["green.200, pink.500", "gray.300, yellow.400", "teal.300, yellow.400", "blue.200, teal.500", "orange.100, purple.300"]
-	const takeColors = ["teal.500, green.500", "gray.500, yellow.200", "green.500, pink.200", "teal.400, yellow.300", "blue.500, teal.200"]
+
 	return (
 		<div>
 			{NftData.map((nftDetail) => {
@@ -16,38 +18,84 @@ export default function NftList () {
 					<Grid>
 					<Center>
 					<GridItem>
-						<Box w="70vw" h="20vh" borderWidth="1px" borderRadius="lg"	mt="15px" mb="15px" color="white">
-							<Center>
-								<SimpleGrid columns={2} spacing='10px'>
+						<Box 
+							w="70vw" 
+							h="20vh" 
+							borderWidth="1px" 
+							borderRadius="lg"	
+							mt="15px" mb="15px" 
+							color="white"
+							p="1rem"
+							backgroundColor="rgba(255, 255, 255, 0.05)"
+						>
+							
+							<Center >
+							<SimpleGrid columns={5} spacing='.25rem' m='1rem'>
+
+								<Center>
+								<SimpleGrid column={1}>
+								{take.map((takeDetails) => {
+									return (
+										<ul>
+											<li>{takeDetails.amount || "1"} {takeDetails.tokenName}</li>
+										</ul>
+									)
+								})}
+								</SimpleGrid>
+								</Center>
+							
+								<Center>
+								<SimpleGrid columns={2} spacing='.15rem'>
 									{take.map((takeOrder, index) => {
-										const boxSize = take.length > 1 ? '40px' : '90px'
+										const boxSize = take.length > 1 ? '2.5rem' : '5.5rem'
 
 										return (index < 4) && (
 											<>
-												{/* <p>{takeOrder.tokenName}</p> */}
-												<Box minHeight={boxSize} width={boxSize} borderRadius="8px" bgGradient={`linear(to-r, ${makeColors[index]})`} />
+												<Box minHeight={boxSize} width={boxSize} borderRadius="8px" bgGradient={`linear(to-br, ${makeColors[index]})`} />
 											</>
 										)
 									})}
-									
+									{take.length > 4 && <p>+{take.length - 4}</p>}
 								</SimpleGrid>
+								</Center>
 
-								<Box width={{ md: 40 }} alignContent="center">
-										<Circle size="40px" bg="white" />
-								</Box>
+								<Center>
+								<SimpleGrid columns={1} m="1rem">
+									<Box width="auto">
+											<Circle size="40px" bg="white"></Circle>
+									</Box>
+								</SimpleGrid>
+								</Center>
 								
-								<SimpleGrid columns={2} spacing='10px'>
+								<Center>
+								<SimpleGrid columns={2} spacing='.15rem'>
 									{make.map((makeOrder, index) => {
-										const boxSize = make.length > 1 ? '40px' : '90px'
+										const boxSize = make.length > 1 ? '2.5rem' : '5.5rem'
 
 									  return (index < 4) && (
 											<>
-												<Box minHeight={boxSize} width={boxSize} borderRadius="8px" bgGradient={`linear(to-l, ${takeColors[index]})`} />
-												{/* <p>{makeOrder.tokenName}</p> */}
+												<Box minHeight={boxSize} width={boxSize} borderRadius="8px" bgGradient={`linear(to-br, ${takeColors[index]})`} />
 											</>
 										)
 									})}
+									{make.length > 4 && <p>+{make.length - 4}</p>}
 								</SimpleGrid>
+								</Center>
+								
+								<Center>
+								<SimpleGrid column={1}>
+								{make.map((makeDetails) => {
+									return (
+										<ul>
+											<li>{makeDetails.amount || "1"} {makeDetails.tokenName}</li>
+										</ul>
+									)
+								})}
+								</SimpleGrid>
+								</Center>
+								
+							</SimpleGrid>
+
 							</Center>
 						</Box>
 					</GridItem>
